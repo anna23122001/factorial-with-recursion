a3b49e3f4dea4788b9c04b9788c791758a65394b
anna <anna.riazantseva123321@gmail.com>
2023-05-18 17:10:10 +0300

    new version 2.0

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict'
 function getFactorial(factorial){
-    if(factorial === 0){
-        return 1
-    } else if (factorial > 1)
+    if(factorial <= 0){
+        return 1;
+    } 
     return factorial * getFactorial(factorial-1);
 }
 console.log(getFactorial(-5))
