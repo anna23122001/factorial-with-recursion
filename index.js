ddee5cdd97036f17c67f07152435624d2f2db830
anna <anna.riazantseva123321@gmail.com>
2023-05-18 15:04:11 +0300

    created function

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict'
+function getFactorial(factorial){
+    if(factorial === 0){
+        return 1
+    }
+    return factorial * getFactorial(factorial-1);
+}
+console.log(getFactorial(5))
