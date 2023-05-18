7fe01103a74128a2551021cd011c6f2afa7f7ae3
anna <anna.riazantseva123321@gmail.com>
2023-05-18 17:04:13 +0300

    new version

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function getFactorial(factorial){
     if(factorial === 0){
         return 1
-    }
+    } else if (factorial > 1)
     return factorial * getFactorial(factorial-1);
 }
-console.log(getFactorial(5))
+console.log(getFactorial(-5))
